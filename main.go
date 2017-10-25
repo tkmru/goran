9a2f55e20992743cfd9d9f929394eedd93822873
tkmru <i.am.tkmru@gmail.com>
2017-10-25 17:57:41 +0900

    cmd line opt parse

--- main.go
+++ main.go
@@ -1,21 +1,36 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 )
 
-func main() {
-    http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request){
-        log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
-        http.ServeFile(w, r, "."+r.URL.Path)
-    })
+var port int
+var addr string
 
-    log.Printf("Start Goran HTTP Server")
-    addr := "127.0.0.1:8080"
-    log.Printf("http://%s", addr)
-    err := http.ListenAndServe(addr, nil)
-    if err != nil {
-        log.Fatal(err)
-    }
+func init() {
+	const defaultPort = 8888
+	flag.IntVar(&port, "port", defaultPort, "port to use")
+	flag.IntVar(&port, "p", defaultPort, "port to use (short)")
+	const defaultAddr = "127.0.0.1"
+	flag.StringVar(&addr, "address", defaultAddr, "address to use")
+	flag.StringVar(&addr, "a", defaultAddr, "address to use (short)")
+	flag.Parse()
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+		http.ServeFile(w, r, "."+r.URL.Path)
+	})
+
+	log.Printf("Start Goran HTTP Server")
+	addr = fmt.Sprintf("%s:%d", addr, port)
+	log.Printf("http://%s", addr)
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
